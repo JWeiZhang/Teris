0c6182e5e831f020ef2dc064f6ede8f9dc63fdb3
Ｗei <howard83124@gmail.com>
2018-02-06 15:16:47 +0800

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GamePageComponent } from './game-page/game-page.component';
+import { GameBlockComponent } from './game-block/game-block.component';
+import { GameScoreComponent } from './game-score/game-score.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePageComponent,
+    GameBlockComponent,
+    GameScoreComponent
   ],
   imports: [
     BrowserModule
